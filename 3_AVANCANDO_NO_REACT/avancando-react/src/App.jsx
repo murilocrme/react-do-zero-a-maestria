c0cb4af209e656c4d8d8d655ed6fa2f3c29d667f
murilocrme <95890358+murilocrme@users.noreply.github.com>
2023-11-06 13:12:08 -0300

    feature
    feature

--- 3_AVANCANDO_NO_REACT/avancando-react/src/App.jsx
+++ 3_AVANCANDO_NO_REACT/avancando-react/src/App.jsx
@@ -7,11 +7,18 @@ import ShowUserName from "./components/ShowUserName";
 
 import City from "./assets/city.jpg";
 import { useState } from "react";
+import CarDetails from "./components/CarDetails";
 
 function App() {
   const name = "Murilo";
 
   const [userName] = useState("Murilo");
+
+  const cars = [
+    { id: 1, brand: "Ferrari", color: "Amarela", newCar: true, km: 0 },
+    { id: 2, brand: "KIA", color: "Amarela", newCar: false, km: 23232 },
+    { id: 3, brand: "Bugatti", color: "Amarela", newCar: true, km: 0 },
+  ];
   return (
     <div>
       <h1>Avançando no React</h1>
@@ -25,7 +32,23 @@ function App() {
       <ManageData />
       <ListRender />
       <ConditionalRender />
+      {/*props*/}
       <ShowUserName name={userName} />
+      {/*destructuring */}
+      <CarDetails brand="VW" km={1000000} color="Azul" newCar={false} />
+      {/* Reaproveitamento*/}
+      <CarDetails brand="BMW" color="red" km={2} newCar={false} />
+      <CarDetails brand="Fiat" color="red" km={2323} newCar={true} />
+      {/*loop em array de objetos */}
+
+      {cars.map((car) => (
+        <CarDetails
+          brand={car.brand}
+          color={car.color}
+          km={car.km}
+          newCar={car.newCar}
+        />
+      ))}
     </div>
   );
 }
